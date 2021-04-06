93df02be2ca2c08616499661297018fe2124dc6e
Siya <67198163+Mar-Issah@users.noreply.github.com>
2021-04-05 20:37:37 -0400

    Update index.js file

--- index.js
+++ index.js
@@ -2,18 +2,9 @@
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
-//ad event listener on the toggle bar
+//toggle to add/remove the show-links class
 navToggle.addEventListener("click", () => { 
-    //just trying to see if you can access the class list {"links",value:"links"} add another class called show-links
-    // console.log(links.classList);
-
-    // if (links.classList.contains("show-link")) {
-    //     links.classList.remove("show-links");
-    // } else {
-    //     links.classList.add("show-links");
-    // }
-    
-    // simple way of adding and removing the show links class when tpggle button is clicked
+   
     links.classList.toggle("show-links");
     
     
